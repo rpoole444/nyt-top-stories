fa0615ce4b8c9f3f67fa1911501e94fb872223e0
Reid Poole <poole.reid@gmail.com>
2023-04-19 12:57:46 -0500

    Add Style and Go Back button to articleDetails component

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ArticleDetails.css";
+import { useHistory } from "react-router-dom";
 
 const ArticleDetails = (props) => {
   const { articles, id } = props;
@@ -7,10 +8,26 @@ const ArticleDetails = (props) => {
   const selectedArticle = articles?.find((el, index) => index === Number(id));
 
   console.log(selectedArticle);
+  const history = useHistory();
+  const goBack = () => {
+    history.goBack();
+  };
   return (
-    <section>
-      <h2>ArticleDetails</h2>
-    </section>
+    <>
+      <section>
+        <h2>{selectedArticle.title}</h2>
+        <p>Category: {selectedArticle.section}</p>
+        <img
+          src={selectedArticle.multimedia[0]?.url}
+          alt={selectedArticle.subsection}
+        />
+        <p>Photo Copyright by: {selectedArticle.multimedia[0].copyright}</p>
+        <p>{selectedArticle.multimedia[0].caption}</p>
+        <p>{selectedArticle.abstract}</p>
+        <p>Written By: {selectedArticle.byLine}</p>
+      </section>
+      <button onClick={() => goBack()}>Go Back</button>
+    </>
   );
 };
 
