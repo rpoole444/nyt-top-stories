f4703760641114a0f5f0640f8f8a8e5708ac9f0b
Reid Poole <poole.reid@gmail.com>
2023-04-19 10:48:32 -0500

    Make dynamic fetch call for the NYT categories

--- src/components/apiCalls.js
+++ src/components/apiCalls.js
@@ -1,9 +1,9 @@
 const NYT_TOP_STORIES_API_KEY = process.env.REACT_APP_NYT_TOP_STORIES_API_KEY;
 
 export const apiCalls = {
-  getArticles() {
+  getArticles(category) {
     return fetch(
-      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${NYT_TOP_STORIES_API_KEY}`
+      `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${NYT_TOP_STORIES_API_KEY}`
     ).then((res) => {
       if (res.ok) {
         return res.json();
