9674bd4d49dd381a308faf45a4ebd76f6e951b88
Reid Poole <poole.reid@gmail.com>
2023-04-19 11:24:06 -0500

    add determine render to render cards and also add conditional render for if there are articles then determine render

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Library.css";
+import ArticleCard from "../ArticleCard/ArticleCard";
 
-const Library = () => {
+const Library = ({ articles }) => {
+  const articleCards = articles?.map((el) => (
+    <ArticleCard key={el.url} {...el} />
+  ));
+
+  const determineRender = () => {
+    return articleCards.length > 0 ? (
+      articleCards
+    ) : (
+      <p>No Articles Available</p>
+    );
+  };
+
   return (
     <section>
       <h2>Library</h2>
+      {articles && determineRender()}
     </section>
   );
 };
