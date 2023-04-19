5a7fc917ceb461562e456ef4dc4ab56fcc873897
Reid Poole <poole.reid@gmail.com>
2023-04-19 10:50:32 -0500

    Add hook for category, add a function for the categoryChange that sets the new category and sets the fetch to false so that you can refetch the material. Then Passing the function as props to the search bar to keep track of the input value

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "./NewsFeed.css";
 import { apiCalls } from "../apiCalls";
+import SearchBar from "../SearchBar/SearchBar";
+import Library from "../Library/Library";
 
 const NewsFeed = () => {
   const [fetching, setFetch] = useState(false);
   const [articles, setArticles] = useState([]);
+  const [category, setCategory] = useState("home");
 
   useEffect(() => {
+    console.log(category);
     if (!fetching) {
-      apiCalls.getArticles().then((data) => {
-        console.log(data);
+      apiCalls.getArticles(category).then((data) => {
+        setArticles(data.results);
+        console.log("data", data);
+        setFetch(true);
       });
-      setFetch(true);
     }
-  }, [fetching]);
+  }, [fetching, category]);
+  console.log("Results: ", articles);
 
+  const onCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+    setFetch(false);
+  };
   return (
     <section>
       <h2>NewsFeed</h2>
+      <SearchBar onCategoryChange={onCategoryChange} />
+      <Library />
     </section>
   );
 };
