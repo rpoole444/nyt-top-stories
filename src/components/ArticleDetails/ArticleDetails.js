47a17fadc1b586a33b82aab9c7552a020eb6f470
Reid Poole <poole.reid@gmail.com>
2023-04-19 13:22:57 -0500

    Add Error handling if there is no article, and a goBack method

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ArticleDetails.css";
 import { useHistory } from "react-router-dom";
 
 const ArticleDetails = (props) => {
   const { articles, id } = props;
-  console.log({ id, articles });
-  const selectedArticle = articles?.find((el, index) => index === Number(id));
 
+  const selectedArticle = articles?.find((el, index) => index === Number(id));
   console.log(selectedArticle);
+
   const history = useHistory();
+
   const goBack = () => {
     history.goBack();
   };
+
+  if (!selectedArticle) {
+    return (
+      <div>
+        <h2>Article Not Found</h2>
+        <button onClick={() => goBack()}>Go Back</button>
+      </div>
+    );
+  }
+
   return (
     <>
-      <section>
+      <section className="article-container">
         <h2>{selectedArticle.title}</h2>
         <p>Category: {selectedArticle.section}</p>
         <img
           src={selectedArticle.multimedia[0]?.url}
-          alt={selectedArticle.subsection}
+          alt={selectedArticle.multimedia[0].caption}
         />
         <p>Photo Copyright by: {selectedArticle.multimedia[0].copyright}</p>
-        <p>{selectedArticle.multimedia[0].caption}</p>
         <p>{selectedArticle.abstract}</p>
+        <p>
+          Visit the NYT's Article:{" "}
+          <a href={selectedArticle.short_url} target="_blank">
+            HERE
+          </a>
+        </p>
         <p>Written By: {selectedArticle.byLine}</p>
       </section>
       <button onClick={() => goBack()}>Go Back</button>
