d8be9995e4e3443c2aa0986ec51fecdd5e99ff2e
Reid Poole <poole.reid@gmail.com>
2023-04-19 10:47:45 -0500

    Add Library component

---
+++ src/components/Library/Library.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Library.css";
+
+const Library = () => {
+  return (
+    <section>
+      <h2>Library</h2>
+    </section>
+  );
+};
+
+export default Library;
