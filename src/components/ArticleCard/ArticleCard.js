906959ae689568e79ca77156d1648b641fd54b64
Reid Poole <poole.reid@gmail.com>
2023-04-19 12:35:09 -0500

    Add Link Route for Artist Details based on the props.id

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ArticleCard.css";
+import { Link } from "react-router-dom";
 
 const ArticleCard = (props) => {
   let imageUrl = "";
@@ -10,16 +11,18 @@ const ArticleCard = (props) => {
     imageCaption = props.multimedia[0].caption;
   }
   return (
-    <div className="article-card">
-      <div className="card-image">
-        {imageUrl && (
-          <img className="card-image" src={imageUrl} alt={imageCaption} />
-        )}
+    <Link to={`/article/${props.id}`}>
+      <div className="article-card">
+        <div className="card-image">
+          {imageUrl && (
+            <img className="card-image" src={imageUrl} alt={imageCaption} />
+          )}
+        </div>
+        <h2 className="article-title">{props.title}</h2>
+        <p className="article-section">Section: {props.section}</p>
+        <p className="article-subsection">{props.subsection}</p>
       </div>
-      <h2 className="article-title">{props.title}</h2>
-      <p className="article-section">Section: {props.section}</p>
-      <p className="article-subsection">{props.subsection}</p>
-    </div>
+    </Link>
   );
 };
 
