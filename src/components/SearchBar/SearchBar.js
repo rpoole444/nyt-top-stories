6e2944e1965ae4cbbeab80887a9b4c4c2fba5f37
Reid Poole <poole.reid@gmail.com>
2023-04-19 14:10:48 -0500

    Finishing touches on functionality for search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./SearchBar.css";
 
-const SearchBar = ({ onCategoryChange }) => {
+const SearchBar = ({ onCategoryChange, onSearchTermChange, searchTerm }) => {
   const handleCategoryChange = (e) => {
     onCategoryChange(e.target.value);
+  };
+
+  const handleSearchInputChange = (e) => {
+    onSearchTermChange(e.target.value);
   };
 
   return (
     <section>
       <div>
+        <input
+          type="text"
+          value={searchTerm}
+          placeholder="search for keyword"
+          onChange={(e) => handleSearchInputChange(e)}
+        ></input>
         <select onChange={(e) => handleCategoryChange(e)}>
           <option value="home">Home</option>
           <option value="arts">Arts</option>
