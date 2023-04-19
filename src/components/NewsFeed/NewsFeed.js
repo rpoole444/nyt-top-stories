61ea12e07d058576e36ba1da7ac81f5939f765cc
Reid Poole <poole.reid@gmail.com>
2023-04-19 12:14:46 -0500

    Change State from here to APP edit props

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,31 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./NewsFeed.css";
-import { apiCalls } from "../apiCalls";
 import SearchBar from "../SearchBar/SearchBar";
 import Library from "../Library/Library";
 
-const NewsFeed = () => {
-  const [fetching, setFetch] = useState(false);
-  const [articles, setArticles] = useState([]);
-  const [category, setCategory] = useState("home");
-
-  useEffect(() => {
-    console.log(category);
-    if (!fetching) {
-      apiCalls.getArticles(category).then((data) => {
-        setArticles(data.results);
-        console.log("data", data);
-        setFetch(true);
-      });
-    }
-  }, [fetching, category]);
-  console.log("Results: ", articles);
-
-  const onCategoryChange = (newCategory) => {
-    setCategory(newCategory);
-    setFetch(false);
-  };
-
+const NewsFeed = ({ onCategoryChange, articles }) => {
   return (
     <section>
       <h2>NewsFeed</h2>
